b227fd1346816df4b2ad5ef9d5c02643e00f5948
Cleingoun <sebastian-richard@gmx.at>
2022-12-06 18:00:27 +0100

    Inital commit

---
+++ src/main/java/at/fhtw/kaipel/slm_colors/SlmColorsApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.kaipel.slm_colors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmColorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmColorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtw/kaipel/slm_colors/SlmColorsApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.kaipel.slm_colors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmColorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
